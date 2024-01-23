880748ad1467bbdd7eb2b3de0d2b353d41b08f91
Leaked method <129183717+leaked-method@users.noreply.github.com>
2024-01-23 16:34:52 +0600

    Update duck.js

--- duck.js
+++ duck.js
@@ -1,4 +1,4 @@
 var logoText = "Ariana";
 var siteName = "Ariana";
-var bestLink = "https://duckist.me";
+var bestLink = "https://www.toprevenuegate.com/mp51gxrspk?key=25fa6f3a279c560a0a4bf2dc70f600bb";
 document.title = "Click Here to watch";
